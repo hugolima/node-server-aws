52a9f16d2eeed0d75bdad569ba79a1c614af2f15
Hugo Lima <hugo.lima@serpro.gov.br>
2023-11-10 00:04:10 -0300

    Alterando para Pool

--- server.mjs
+++ server.mjs
@@ -1,8 +1,8 @@
 import express  from 'express'
 import * as pg from 'pg'
 
-const { Client } = pg.default
-const client = new Client({
+const { Pool } = pg.default
+const client = new Pool({
   user: 'postgres',
   host: 'database-pos-ceub.c7fouuzhnnjw.us-east-2.rds.amazonaws.com',
   database: 'postgres',
@@ -13,6 +13,9 @@ const client = new Client({
   },
 })
 
+console.log('Conectando ao banco database-pos-ceub')
+await client.connect()
+
 const app = express()
 const port = 3000
 
@@ -20,12 +23,10 @@ app.use(express.json())
 
 app.get('/usuarios', async (req, res) => {
   try {
-    console.log('Conectando ao banco database-pos-ceub')
-    await client.connect()
     const lista = await client.query('SELECT * FROM Usuario')
     res.send(lista.rows)
   } finally {
-    client.end();
+    client.release();
   }
 })
 
@@ -33,12 +34,9 @@ app.post('/usuarios', async (req, res) => {
   const usuario = req.body;
 
   try {
-    console.log('Conectando ao banco database-pos-ceub')
-    await client.connect()
-
     await client.query('BEGIN')
-    const queryText = 'INSERT INTO Usuario(nome, sexo, idade) VALUES($1, $2, $3) RETURNING id'
-    const res = await client.query(queryText, [usuario.nome, usuario.sexo, usuario.idade])
+    const queryText = 'INSERT INTO Usuario(nome, sexo, idade) VALUES($1, $2, $3) RETURNING id;'
+    const res = await client.query(queryText, ['' + usuario.nome, '' + usuario.sexo, usuario.idade])
     const idRetorno = res.rows[0].id
     await client.query('COMMIT')
 
@@ -49,7 +47,7 @@ app.post('/usuarios', async (req, res) => {
     await client.query('ROLLBACK')
     throw e
   } finally {
-    client.end()
+    client.release()
   }
 })
 
